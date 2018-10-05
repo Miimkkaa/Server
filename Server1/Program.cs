ee484fa525abb0d1c4c873933637040a0116c8e0
Milena Ognyanova <mile0416@edu.easj.dk>
2018-10-05 13:11:35 +0200

    mimi

--- Server1/Program.cs
+++ Server1/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using DLLfile;
 
 namespace Server1
 {
     class Program
-    {
-        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            private static readonly int PORT = 7;
+            static void Main(string[] args)
+            {
+                IPAddress localAddress = IPAddress.Loopback;
+                TcpListener serverSocket = new TcpListener(localAddress, PORT);
+                serverSocket.Start();
+                Console.WriteLine("TCP Server running on port" + PORT);
+
+                while (true)
+                {
+                    try
+                    {
+                        TcpClient client = serverSocket.AcceptTcpClient();
+                        Console.WriteLine("Incoming client");
+                        Task.Run(() => Weight(client));
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine("Exception will continue");
+                    }
+
+                }
+            }
+            private static void Weight(TcpClient client)
+            {
+                NetworkStream stream = client.GetStream();
+                StreamReader reader = new StreamReader(stream);
+                StreamWriter writer = new StreamWriter(stream);
+                while (true)
+                {
+                    string request = reader.ReadLine();
+                    if (request == "Bye") break;
+                    Console.WriteLine("Request: " + request);
+
+                    string[] values = request.Split(" ");
+                    string function = (string)values[0];
+                    double x = Double.Parse(values[1]);
+                    double response = 0;
+
+                    Converting cv = new Converting();
+                    if (function == "ConvertToGrams")
+                    {
+                        response = cv.ConvertToGrams(x);
+                    }
+                    else if (function == "ConvertToOunces")
+                    {
+                        response = cv.ConvertToOunces(x);
+                    }
+
+                    Console.WriteLine("Response: " + response);
+                    writer.WriteLine(response);
+                    writer.Flush();
+                }
+                client.Close();
+            }
         }
     }
-}
